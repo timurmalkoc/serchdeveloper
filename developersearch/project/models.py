239426b6b89c529a24dff2e3e2ce071e622ef85e
timurmalkoc <malkoctimurcelik@gmail.com>
2022-10-15 14:57:41 -0400

    db update

--- developersearch/project/models.py
+++ developersearch/project/models.py
@@ -1,6 +1,7 @@
 from email.policy import default
 from enum import unique
 from pyexpat import model
+from secrets import choice
 from turtle import title
 from unittest.util import _MAX_LENGTH
 from django.db import models
@@ -11,6 +12,38 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     desciption = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
+
+
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- developersearch/project/views.py
+++ developersearch/project/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project
 
 def projects(request):
+    projects = Project.objects.all()
     return HttpResponse('Here are products')
+
+
+def project(request, pk):
+    project = Project.objects.get(id=pk)
+
+
+def createProject(request):
+    pass
+
+def updateProject(request):
+    pass
+
+
+def deleteProject(request):
+    pass
