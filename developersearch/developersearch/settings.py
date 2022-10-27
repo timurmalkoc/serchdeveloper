0c046a9b00b2b6fc673b56d27547b4278e764c79
timurmalkoc <malkoctimurcelik@gmail.com>
2022-10-27 17:48:32 -0400

    upload image pillow ext

--- developersearch/developersearch/settings.py
+++ developersearch/developersearch/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -15,6 +15,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Application definition
 
