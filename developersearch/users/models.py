40b93fe4f2604e2596054667efed0c85cc136f25
timurmalkoc <malkoctimurcelik@gmail.com>
2022-10-30 13:31:29 -0400

    user skills model

--- developersearch/users/models.py
+++ developersearch/users/models.py
@@ -12,6 +12,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=(200), blank=True, null=True)
     email = models.EmailField(max_length=(500), blank=True, null=True)
     username = models.CharField(max_length=(200), blank=True, null=True)
+    location = models.CharField(max_length=(200), blank=True, null=True)
     short_intro = models.CharField(max_length=(200), blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(blank=True, null=True, upload_to='profiles/', default="profiles/user-default.png")
@@ -25,3 +26,13 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return str(self.user.username)
+
+class Skills(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=(200), blank=True, null=True)
+    desciption = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add = True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.name)
