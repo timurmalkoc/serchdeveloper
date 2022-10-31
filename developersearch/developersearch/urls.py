4ca488ad5d1f388dd864053000053a5a2dfc9818
timurmalkoc <malkoctimurcelik@gmail.com>
2022-10-31 17:08:00 -0400

    api project end-points

--- developersearch/developersearch/urls.py
+++ developersearch/developersearch/urls.py
@@ -5,10 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('project.urls')),
-    path('users/', include('users.urls')),
+    path('api/', include('api.urls')),
 ]
 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- developersearch/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import URLPattern, path
-from . import views
-
-urlpatterns = [
-    path('login/', views.loginPage, name="login"),
-    path('', views.profiles, name='profiles'),
-    path('profile/<str:pk>/', views.userProfile, name='user-profile')
-]
