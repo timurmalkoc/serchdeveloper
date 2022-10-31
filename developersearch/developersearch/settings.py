57ce41e710d2f3430b83ca8d8397424b022b2a89
timurmalkoc <malkoctimurcelik@gmail.com>
2022-10-31 12:54:15 -0400

    welcome email

--- developersearch/developersearch/settings.py
+++ developersearch/developersearch/settings.py
@@ -115,6 +115,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
--- developersearch/users/signals.py
+++ developersearch/users/signals.py
@@ -1,8 +1,12 @@
+from email import message
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from django.contrib.auth.models import User
 from .models import Profile
+
+from django.core.mail import send_mail
+from django.conf import settings
 
 # === anytime when we create a user, it will create profile authomaticly
 #@receiver(post_save, sender=Profile)
@@ -15,6 +19,20 @@ def createProfile(sender, instance, created, **kwargs):
             email = user.email,
             name = user.first_name,
         )
+
+        # send welcome message
+
+        # subject = 'Welcome to DeveloperSearch'
+        # message = 'We are glad you are here!'
+
+        # send_mail(
+        #     subject,
+        #     message,
+        #     settings.EMAIL_HOST_USER,
+        #     [profile.email],
+        #     fail_silently=False
+        # )
+        
 #==== if profile delete, user will delete by delete signal
 #@receiver(post_delete, sender=Profile)
 def deleteUser(sender, instance, **kwargs):
